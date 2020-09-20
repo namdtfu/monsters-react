f25324f022c8a59f9e56ca78b0f5a7b3bfca5168
Christian <snamun1609@gmail.com>
2020-09-20 16:47:16 +0700

    Updates

---
+++ precache-manifest.9b1f4f66260cbca6dcaa2802c72b4977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c38b4b58eb8a278567bab6b42ac3718",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "b05e1303be4fee13a6da",
+    "url": "/monsters-react/static/css/main.d6741b0d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-react/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b05e1303be4fee13a6da",
+    "url": "/monsters-react/static/js/main.1f02252a.chunk.js"
+  },
+  {
+    "revision": "52d0e7f35021692ce0ba",
+    "url": "/monsters-react/static/js/runtime-main.2e36a746.js"
+  }
+]);
